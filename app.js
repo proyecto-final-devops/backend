af81127a935845f3a761eb6db30aa65d6d7341ed
Brandon A <106130946+Traxcomgarza@users.noreply.github.com>
2025-10-02 23:41:21 -0600

    pruebas unitarias

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const productosRoutes = require('./routes/productRoutes');
 const usuariosRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: 'http://localhost:4200', 
@@ -17,17 +16,11 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/auth', usuariosRoutes);
-                                                  
-app.use('/api/usuarios', usuariosRoutes);                                                                                          
-
+app.use('/api/usuarios', usuariosRoutes);
 app.use('/api/productos', productosRoutes);
 
 app.get('/', (req, res) => {
   res.send('API de productos funcionando');
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
 module.exports = app;
